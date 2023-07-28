9b9ed4597e59056ac524f3cee4beaac8f1f18731
박주현 <114227320+Programmer-may@users.noreply.github.com>
2023-07-28 16:38:21 +0900

    [Silver IV] Title: ATM, Time: 144 ms, Memory: 14436 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for(int i = 0; i < N; i++) {
+            int p = Integer.parseInt(st.nextToken());
+            map.put(i,p);
+        }
+        List<Integer> list = new ArrayList<Integer>(map.values());
+        Collections.sort(list);
+        int sum=0;
+
+        for (int i = 0; i < list.size(); i++) {
+            sum += list.get(i)*(list.size()-i);
+        }
+        System.out.println(sum);
+    }
+}
