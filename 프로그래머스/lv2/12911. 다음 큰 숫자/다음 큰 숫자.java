92a52241b95d0083e9797c5d23115f8246d3d1f5
박주현 <114227320+Programmer-may@users.noreply.github.com>
2023-09-12 23:31:44 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.04 ms, Memory: 52 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int cntZeroOfN = countOne(binaryConversion(n));
+        for (int i = n + 1; i < Integer.MAX_VALUE; i++) {
+            if (countOne(binaryConversion(i)) == cntZeroOfN) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+
+    public static String binaryConversion(int num) {
+        String binaryNum = Integer.toBinaryString(num);
+        return binaryNum;
+    }
+
+    public static int countOne(String binaryNum) {
+        int cntOne = 0;
+        for (int i = 0; i < binaryNum.length(); i++) {
+            if (binaryNum.charAt(i) == '1') {
+                cntOne++;
+            }
+        }
+        return cntOne;
+    }
+}
