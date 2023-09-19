553180953054efa4fbf5696adad560c2e852a082
박주현 <114227320+Programmer-may@users.noreply.github.com>
2023-09-19 17:47:45 +0900

    [unrated] Title: 공원 산책, Time: 1.43 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/unrated/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.java
@@ -2,82 +2,46 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(String[] park, String[] routes) {
-        String[][] parkMatrix = new String[park.length][park[0].length()];
-        int x = 0;
-        int y = 0;
-        int horizonMin = 0;
-        int verticalMin = 0;
+        char[][] parkMatrix = new char[park.length][park[0].length()];
+        int[] dr = {-1, 0, 1, 0};
+        int[] dc = {0, 1, 0, -1};
+        char[] dir = {'N', 'E', 'S', 'W'};
+        int startR = 0;
+        int startC = 0;
         int horizonMax = park[0].length() - 1;
         int verticalMax = park.length - 1;
         for (int i = 0; i < park.length; i++) {
             String[] val = park[i].split("");
             for (int j = 0; j < val.length; j++) {
-                parkMatrix[i][j] = val[j];
-                if (val[j].equals("S")) {
-                    x = j;
-                    y = i;
+                parkMatrix[i][j] = val[j].charAt(0);
+                if (val[j].charAt(0) == 'S') {
+                    startR = i;
+                    startC = j;
                 }
             }
         }
         for (int i = 0; i < routes.length; i++) {
-            String[] val = routes[i].split(" ");
-            String direction = val[0];
-            if (direction.equals("E")) {
-                int cnt = 0;
-                int tmp = x;
-                for (int r = 0; r < Integer.parseInt(val[1]); r++) {
-                    if (tmp + 1 <= horizonMax && !parkMatrix[y][tmp + 1].equals("X")) {
-                        cnt++;
+            char cmd = routes[i].split(" ")[0].charAt(0);
+            int distance = Integer.parseInt(routes[i].split(" ")[1]);
+            for (int k = 0; k < 4; k++) {
+                if (cmd == dir[k]) {
+                    int nc = startC;
+                    int nr = startR;
+                    int tmp = 0;
+                    for (int t = 0; t < distance; t++) {
+                        nr = nr + dr[k];
+                        nc = nc + dc[k];
+                        if (nr < 0 || nr > verticalMax || nc < 0 || nc > horizonMax || parkMatrix[nr][nc] == 'X')
+                            continue;
                         tmp++;
                     }
-                }
-                if (cnt == Integer.parseInt(val[1])) {
-                    x += Integer.parseInt(val[1]);
-                }
-
-            } else if (direction.equals("W")) {
-                int cnt = 0;
-                int tmp = x;
-                for (int r = 0; r < Integer.parseInt(val[1]); r++) {
-                    if (tmp - 1 >= horizonMin && !parkMatrix[y][tmp - 1].equals("X")) {
-                        cnt++;
-                        tmp--;
+                    if (tmp == distance) {
+                        startR = nr;
+                        startC = nc;
                     }
-                }
-                if (cnt == Integer.parseInt(val[1])) {
-                    x -= Integer.parseInt(val[1]);
-                }
-
-            } else if (direction.equals("S")) {
-                int cnt = 0;
-                int tmp = y;
-                for (int r = 0; r < Integer.parseInt(val[1]); r++) {
-                    if (tmp + 1 <= verticalMax && !parkMatrix[tmp + 1][x].equals("X")) {
-                        cnt++;
-                        tmp++;
-                    }
-                }
-                if (cnt == Integer.parseInt(val[1])) {
-                    y += Integer.parseInt(val[1]);
-                }
-
-            } else if (direction.equals("N")) {
-                int cnt = 0;
-                int tmp = y;
-                for (int r = 0; r < Integer.parseInt(val[1]); r++) {
-                    if (tmp - 1 >= verticalMin && !parkMatrix[tmp - 1][x].equals("X")) {
-                        cnt++;
-                        tmp--;
-                    }
-                }
-                if (cnt == Integer.parseInt(val[1])) {
-                    y -= Integer.parseInt(val[1]);
                 }
             }
         }
-        int[] answer = new int[2];
-        answer[0] = y;
-        answer[1] = x;
-        return answer;
+        return new int[]{startR, startC};
     }
 }
