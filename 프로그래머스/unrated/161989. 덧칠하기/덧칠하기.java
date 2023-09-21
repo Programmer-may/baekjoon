e6857637895032d690bee3199402c4a3b7e130d2
박주현 <114227320+Programmer-may@users.noreply.github.com>
2023-09-21 18:24:08 +0900

    [unrated] Title: 덧칠하기, Time: 6.53 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -1,11 +1,10 @@
-import java.util.Stack;
+import java.util.*;
 
 class Solution {
     public int solution(int n, int m, int[] section) {
         Stack<Integer> stack = new Stack<>();
-        int max = n-4;
-        for (int i =0; i<section.length;i++){
-            if (stack.isEmpty() || stack.peek() + m -1<section[i]){
+        for (int i = 0; i < section.length; i++) {
+            if (stack.isEmpty() || stack.peek() + m - 1 < section[i]) {
                 stack.push(section[i]);
             }
         }
