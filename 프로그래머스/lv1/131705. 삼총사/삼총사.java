5e9f42144352f36055851afa7238f874fffd1d43
박주현 <114227320+Programmer-may@users.noreply.github.com>
2023-10-04 18:28:44 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -1,8 +1,8 @@
 class Solution {
     public int solution(int[] number) {
         int answer = 0;
-        for (int x = 0; x < number.length; x++) {
-            for (int y = x + 1; y < number.length; y++) {
+        for (int x = 0; x < number.length - 2; x++) {
+            for (int y = x + 1; y < number.length - 1; y++) {
                 for (int z = y + 1; z < number.length; z++) {
                     if(number[x] + number[y] + number[z] == 0) {
                         answer++;
