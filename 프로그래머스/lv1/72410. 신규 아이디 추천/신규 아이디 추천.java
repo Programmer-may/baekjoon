3f2e94de7e13927f6c36ff3811a128c17e85322f
박주현 <114227320+Programmer-may@users.noreply.github.com>
2023-10-04 17:23:50 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.85 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,26 +1,26 @@
 class Solution {
     public String solution(String new_id) {
-        String answer = new_id.toLowerCase(); // 1단계
-
-        answer = answer.replaceAll("[^-_.a-z0-9]", ""); // 2단계
-        answer = answer.replaceAll("[.]{2,}", "."); // 3단계
-        answer = answer.replaceAll("^[.]|[.]$", "");    // 4단계
-        
-        if (answer.equals("")) {    // 5단계
-            answer += "a";
+        String answer = new_id.toLowerCase();
+        StringBuilder sb = new StringBuilder();
+        //answer = answer.replaceAll("[~!@#$%^&*()=+[{]}:?,<>/]", "")
+        answer = answer.replaceAll("[^0-9a-z-_.]", "")
+                .replaceAll("\\.++", ".");
+        sb.append(answer);
+        if (sb.length() != 0){
+            if (sb.charAt(0) == '.') sb.deleteCharAt(0);
         }
-
-        if (answer.length() >= 16) {     // 6단계
-            answer = answer.substring(0, 15);
-            answer = answer.replaceAll("[.]$","");
+        if (sb.length() != 0){
+            if (sb.charAt(sb.length() - 1) == '.') sb.deleteCharAt(sb.length() - 1);
         }
-
-        if (answer.length() <= 2) {  // 7단계
-            while (answer.length() < 3) {
-                answer += answer.charAt(answer.length()-1);
+        if (sb.length() == 0) sb.append('a');
+        if (sb.length() >= 16) sb.delete(15, sb.length());
+        if (sb.charAt(sb.length() - 1) == '.') sb.deleteCharAt(sb.length() - 1);
+        if (sb.length() <= 2) {
+            char lastChar = sb.charAt(sb.length() - 1);
+            for (int i = sb.length(); i < 3; i++) {
+                sb.append(lastChar);
             }
         }
-
-        return answer;
+        return sb.toString();
     }
 }
