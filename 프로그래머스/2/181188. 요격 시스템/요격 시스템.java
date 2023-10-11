96386739dbf8e8e4e41f0e273cee7b8b2721c212
박주현 <114227320+Programmer-may@users.noreply.github.com>
2023-10-11 19:20:04 +0900

    [level 2] Title: 요격 시스템, Time: 0.43 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] targets) {
+        int answer = 0;
+        Arrays.sort(targets, (o1, o2) -> o1[1] - o2[1]);
+        int currPosition = 0;
+        for (int i = 0; i < targets.length; i++) {
+            if (currPosition <= targets[i][0]) {
+                currPosition = targets[i][1];
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
