53678428fcc79c9a54d6115e2aabc1d514ef83a9
박주현 <114227320+Programmer-may@users.noreply.github.com>
2023-11-03 17:22:55 +0900

    [level 2] Title: 소수 찾기, Time: 1.33 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    static char[] check = new char[7];
+    static StringBuilder sb = new StringBuilder();
+    static int answer = 0;
+    static HashMap<Integer,Integer> map = new HashMap<>();
+    public int solution(String numbers) {
+        char[] numArr = numbers.toCharArray();
+        for (int i = 1; i <= numbers.length(); i++) {
+
+            dfs(0, i, numArr);
+        }
+        return answer;
+    }
+
+    public void dfs(int lev, int i, char[] numArr) {
+        if (lev == i) {
+            int num = Integer.parseInt(sb.toString());
+            if (isPrimeNumber(num) && !map.containsKey(num)) {
+                map.put(num,1);
+                answer++;
+            }
+        } else {
+            for (int j = 0; j < numArr.length; j++) {
+                if (check[j] == 0) {
+                    check[j] = 1;
+                    sb.append(numArr[j]);
+                    dfs(lev + 1, i, numArr);
+                    check[j] = 0;
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+            }
+        }
+    }
+    public boolean isPrimeNumber(int number) {
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return number != 0 && number != 1;
+    }
+}
