850da7f644ba07d103956e70d21f5445ffcd66ee
박주현 <114227320+Programmer-may@users.noreply.github.com>
2023-11-10 17:00:04 +0900

    [level 2] Title: 문자열 압축, Time: 0.04 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(String s) {
+        int answer = s.length();
+
+        for (int i = 1; i <= s.length() / 2; i++) {
+            int len = 0;
+            for (int j = 0; j + i <= s.length(); ) {
+                int h = j + i;
+                int cnt = 1;
+                String seg = s.substring(j, j + i);
+
+                while (h + i <= s.length() && seg.equals(s.substring(h, h + i))) {
+                    h += i;
+                    cnt++;
+                }
+
+                if (cnt == 1) len += i;
+                else len += i + String.valueOf(cnt).length();
+
+                j = h;
+            }
+            if (s.length() % i != 0) len += s.length() % i;
+            answer = Math.min(answer, len);
+        }
+        return answer;
+    }
+}
