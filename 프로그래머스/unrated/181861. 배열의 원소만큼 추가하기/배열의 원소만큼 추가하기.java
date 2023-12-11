8fd5491987877503b4d598de925fc90a0e700cc4
박주현 <114227320+Programmer-may@users.noreply.github.com>
2023-12-11 20:52:24 +0900

    [level unrated] Title: 배열의 원소만큼 추가하기, Time: 0.02 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        int size = 0;
+        for (int num : arr) {
+            size += num;
+        }
+        int[] answer = new int[size];
+        int idx = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i]; j++){
+                answer[idx++] = arr[i];
+            }
+        }
+        return answer;
+    }
+}
