9257bbbad9a8b47d598b1679915e29feb877cdcf
박주현 <parkjh080@naver.com>
2024-04-05 18:49:12 +0900

    [level 2] Title: 가장 큰 수, Time: 0.46 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -8,17 +8,17 @@ class Solution {
             answerArr[i] = String.valueOf(numbers[i]);
         }
         Comparator<String> customComparator = (s1, s2) -> {
-            while (true) {
-                if (s1.length() == s2.length())
+            while(true) {
+                if(s1.length() ==  s2.length())
                     return s2.compareTo(s1);
-                if (s1.length() < s2.length()) {
+                if(s1.length() <  s2.length()) {
                     for (int i = 0; i < s1.length(); i++) {
                         if (s1.charAt(i) != s2.charAt(i))
                             return s2.substring(i, i + 1).compareTo(s1.substring(i, i + 1));
                     }
                     s2 = s2.substring(s1.length());
                 }
-                if (s1.length() > s2.length()) {
+                if(s1.length() >  s2.length()) {
                     for (int i = 0; i < s2.length(); i++) {
                         if (s1.charAt(i) != s2.charAt(i))
                             return s2.substring(i, i + 1).compareTo(s1.substring(i, i + 1));
