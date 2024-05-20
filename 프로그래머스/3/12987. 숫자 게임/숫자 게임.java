5eb789b0a83429186975b9a034bc13b825e56d95
박주현 <parkjh080@naver.com>
2024-05-20 18:50:15 +0900

    [level 3] Title: 숫자 게임, Time: 72.26 ms, Memory: 67.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int x = -1;
+        int answer = 0;
+        for(int i = 0; i < A.length; i++){
+            for(int j = x + 1; j < B.length; j++){
+                if(A[i] < B[j]){
+                    x = j;
+                    answer++;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
