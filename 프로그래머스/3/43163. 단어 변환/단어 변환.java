6c2f208c8ccbdc89188dafba73b13104ae61c453
박주현 <parkjh080@naver.com>
2024-05-27 22:42:15 +0900

    [level 3] Title: 단어 변환, Time: 0.03 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String begin, String target, String[] words) {
+        if (!hasTarget(target, words)) return 0;
+        Queue<String> queue = new LinkedList<>();
+        int answer = 0;
+        queue.add(begin);
+        while (!queue.isEmpty() || answer <= words.length) {
+            int size = queue.size();
+            for(int i = 0; i < size; i++) {
+                String now = queue.poll();
+                if (now.equals(target)) return answer;
+                for (String word : words) {
+                    if (isPossible(now, word)) queue.add(word);
+                }
+            }
+            answer++;
+        }
+        return 0;
+    }
+    
+    boolean hasTarget(String target, String[] words) {
+        for (String word : words) {
+            if (word.equals(target)) return true;
+        }
+        return false;
+    }
+    
+    boolean isPossible(String before, String after) {
+        int len = before.length();
+        int tmp = 0;
+        for (int i = 0; i < len; i++) {
+            if (before.charAt(i) == after.charAt(i)) tmp++;
+        }
+        if (tmp == len - 1) return true;
+        return false;
+    }
+}
