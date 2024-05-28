b49c8c11b6a78a1d065f0b8827681beaf4213c03
박주현 <parkjh080@naver.com>
2024-05-28 18:02:49 +0900

    [level 3] Title: 등굣길, Time: 0.89 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,5 +1,4 @@
 class Solution {
-    final int MAGIC_NUM = 1000000007;
     public int solution(int m, int n, int[][] puddles) {
         int answer = 0;
         int[][] map = new int[n][m];
@@ -12,7 +11,6 @@ class Solution {
                 if (map[i][j] != -1) {
                     if (j >= 1 && map[i][j - 1] != -1) map[i][j] +=  map[i][j - 1];
                     if (i >= 1 && map[i - 1][j] != -1) map[i][j] +=  map[i - 1][j];
-                    if (map[i][j] > MAGIC_NUM) map[i][j] %= MAGIC_NUM;
                 }
             }
         }
