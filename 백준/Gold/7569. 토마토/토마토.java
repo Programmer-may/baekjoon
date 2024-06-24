df0789e17809c09fd09dbb40debff539b3124c4b
박주현 <parkjh080@naver.com>
2024-06-24 12:00:07 +0900

    [Gold V] Title: 토마토, Time: 736 ms, Memory: 151324 KB -BaekjoonHub

---
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] info = br.readLine().split(" ");
+        int y = Integer.parseInt(info[1]);
+        int x = Integer.parseInt(info[0]);
+        int z = Integer.parseInt(info[2]);
+        int[] dy = new int[]{-1, 0, 1, 0, 0, 0};
+        int[] dx = new int[]{0, 1, 0, -1, 0, 0};
+        int[] dz = new int[]{0, 0, 0, 0, 1, -1};
+        int[][][] matrix = new int[y][x][z];
+        boolean[][][] visited = new boolean[y][x][z];
+
+        int totalTomato = 0;
+        int totalBlank = 0;
+        int totalMatured = 0;
+
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int i = 0; i < z; i++) {
+            for (int j = 0; j < y; j++) {
+                String[] row = br.readLine().split(" ");
+                for (int l = 0; l < x; l++) {
+                    if (row[l].equals("1")) {
+                        totalTomato++;
+                        totalMatured++;
+                        matrix[j][l][i] = 1;
+                        queue.add(new int[]{j, l, i});
+                        visited[j][l][i] = true;
+                    } else if (row[l].equals("0")) {
+                        totalTomato++;
+                        matrix[j][l][i] = 0;
+                    } else {
+                        totalBlank++;
+                        matrix[j][l][i] = -1;
+                    }
+                }
+            }
+        }
+        if (totalMatured == 0) {
+            System.out.println(-1);
+            return;
+        } else if (totalTomato == totalMatured) {
+            System.out.println(0);
+            return;
+        }
+        int answer = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] now = queue.poll();
+                for (int k = 0; k < 6; k++) {
+                    int ny = now[0] + dy[k];
+                    int nx = now[1] + dx[k];
+                    int nz = now[2] + dz[k];
+                    if (ny >= 0 && ny < y && nx >= 0 && nx < x && nz >= 0 && nz < z && matrix[ny][nx][nz] != -1 && !visited[ny][nx][nz]) {
+                        queue.add(new int[]{ny, nx, nz});
+                        visited[ny][nx][nz] = true;
+                        totalMatured++;
+                    }
+                }
+            }
+            answer++;
+        }
+        if (totalMatured != totalTomato) System.out.println(-1);
+        else System.out.println(answer - 1);
+    }
+}
