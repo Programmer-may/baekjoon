857efef8b62c67d3d6c109a56afd91a4047c6f2c
박주현(Ju-Hyeon Park) <parkjh080@naver.com>
2024-08-30 12:01:02 +0900

    [level 1] Title: 모의고사, Time: 1.90 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        int oneStudentAnswerCnt = 0;
+        for (int i = 0; i < answers.length; i++) {
+            int answer = answers[i];
+            if (answer == i % 5 + 1) oneStudentAnswerCnt++;
+        }
+        int twoStudentAnswerCnt = 0;
+        for (int i = 0; i < answers.length; i++) {
+            int answer = answers[i];
+            if (i % 2 == 0) {
+                if (answer == 2) twoStudentAnswerCnt++;
+            } else {
+                if (i % 8 == 1 && answer == 1) twoStudentAnswerCnt++;
+                if (i % 8 == 3 && answer == 3) twoStudentAnswerCnt++;
+                if (i % 8 == 5 && answer == 4) twoStudentAnswerCnt++;
+                if (i % 8 == 7 && answer == 5) twoStudentAnswerCnt++;
+            }
+        }
+        int threeStudentAnswerCnt = 0;
+        for (int i = 0; i < answers.length; i++) {
+            int answer = answers[i];
+            if ((i % 10 == 0 || i % 10 == 1) && answer == 3) threeStudentAnswerCnt++;
+            if ((i % 10 == 2 || i % 10 == 3) && answer == 1) threeStudentAnswerCnt++;
+            if ((i % 10 == 4 || i % 10 == 5) && answer == 2) threeStudentAnswerCnt++;
+            if ((i % 10 == 6 || i % 10 == 7) && answer == 4) threeStudentAnswerCnt++;
+            if ((i % 10 == 8 || i % 10 == 9) && answer == 5) threeStudentAnswerCnt++;
+        }
+        int maxAnswerCnt = Math.max(oneStudentAnswerCnt, Math.max(twoStudentAnswerCnt, threeStudentAnswerCnt));
+        List<Integer> answerList = new ArrayList<>();
+        if (maxAnswerCnt == oneStudentAnswerCnt) answerList.add(1);
+        if (maxAnswerCnt == twoStudentAnswerCnt) answerList.add(2);
+        if (maxAnswerCnt == threeStudentAnswerCnt) answerList.add(3);
+        int[] answer = new int[answerList.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = answerList.get(i);
+        }
+        
+        return answer;
+    }
+}
