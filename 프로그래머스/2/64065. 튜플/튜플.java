f9d4d18f8c7aa4ba8c24f93c97576ca82d95b3c9
박주현(Ju-Hyeon Park) <parkjh080@naver.com>
2024-10-17 18:22:10 +0900

    [level 2] Title: 튜플, Time: 46.41 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,37 +2,33 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
-        Map<Integer, Integer> map = new HashMap<>();
-        char[] arr = s.toCharArray();
-        for (int i = 2; i < arr.length; ) {
-            if (isNumber(arr[i])) {
-                StringBuilder sb = new StringBuilder();
-                sb.append(arr[i]);
-                for (int j = i + 1; j < arr.length; j++) {
-                    if (isNumber(arr[j])) sb.append(arr[j]);
-                    else {
+        PriorityQueue<String> pq = new PriorityQueue<>((a, b) -> a.length() - b.length());
+        for (int i = 0; i < s.length(); i++) {
+            if(Character.isDigit(s.charAt(i))) {
+                for (int j = i + 1; j < s.length(); j++) {
+                    if (s.charAt(j) != ',' && !Character.isDigit(s.charAt(j))) {
+                        pq.add(s.substring(i, j));
                         i = j + 1;
                         break;
                     }
                 }
-                int num = Integer.parseInt(sb.toString());
-                map.put(num, map.getOrDefault(num, 0) + 1);
-            } else {
-                i++;
             }
         }
-        int[] answer = new int[map.keySet().size()];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
-        for (int key : map.keySet()) {
-            pq.add(new int[]{key, map.get(key)});
+        List<String> answerList = new ArrayList<>();
+        Set<String> elements = new HashSet<>();
+        while (!pq.isEmpty()) {
+            String[] current = pq.poll().split(",");
+            for (String element : current) {
+                if (!elements.contains(element)) {
+                    answerList.add(element);
+                    elements.add(element);
+                }
+            }
         }
+        int[] answer = new int[answerList.size()];
         for (int i = 0; i < answer.length; i++) {
-            answer[i] = pq.poll()[0];
+            answer[i] = Integer.parseInt(answerList.get(i));
         }
         return answer;
-    }
-
-    private boolean isNumber(char c) {
-        return Character.isDigit(c);
     }
 }
