f945effc08c74ec1767ff8da717563861eb0db2e
박주현(Ju-Hyeon Park) <parkjh080@naver.com>
2024-12-09 22:44:58 +0900

    [level 1] Title: 기사단원의 무기, Time: 229.79 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.kt
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.kt
@@ -3,17 +3,17 @@ class Solution {
         var answer: Int = 0
         for (i in 1..number) {
             val result = countDivisor(i)
-            println(result)
             when {
                 result <= limit -> answer += result
                 else -> answer += power
             }
         }
-        return answer + 1
+        return answer
     }
+    
     fun countDivisor(num: Int): Int {
         val nums = mutableSetOf<Int>()
-        for (i in 1..num / 2) {
+        for (i in 1..Math.sqrt(num.toDouble()).toInt()) {
             when {
                 num % i == 0 -> {
                     nums.add(i)
